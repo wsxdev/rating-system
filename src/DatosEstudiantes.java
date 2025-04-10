37e89fe82799fe17e3df7c3361c9a318d5f7d916
Britanr <redrovanbritay1751623776gmail.com>
2025-04-10 00:27:33 -0500

    Datos.Estudiantes.java "Arreglo en nombres"

--- src/DatosEstudiantes.java
+++ src/DatosEstudiantes.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 
 public class DatosEstudiantes {
     public static void main(String[] args) {
-        Scanner ingreso = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         ///Ingresamos los PAOS
-        System.out.println("Ingrese el PAO a calificar: ");
-        int pao = ingreso.nextInt();
+        System.out.print("Ingrese el PAO a calificar: ");
+        int pao = entrada.nextInt();
 
         /// Ingresamos el numero de estudiantes
-        System.out.println("Ingrese el numero de estudiantes del PAO " + pao + ":");
-        int cantidad = ingreso.nextInt();
+        System.out.print("Ingrese el numero de estudiantes del PAO " + pao + ":");
+        int cantidad = entrada.nextInt();
 
         /// Ingresamos el nombre de los estudiantes
-
         for (int i = 0; i < cantidad; i++) {
-            System.out.println("Ingrese el nombre del estudiante " + (i + 1) + " :");
+            System.out.print("Ingrese el nombre del estudiante " + (i + 1) + ": ");
             String[] nombreEstudiantes = new String[cantidad];
-            String nombre = ingreso.next();
+            nombreEstudiantes[i] = entrada.next();
         }
     }
 }
