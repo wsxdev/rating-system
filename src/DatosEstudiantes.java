2604f57e7906de2c43866dd236816fb3d1fb63b8
Britanr <redrovanbritay1751623776gmail.com>
2025-04-09 22:38:24 -0500

    DatosEstudiantes.java "Cambio en los datos"

--- src/DatosEstudiantes.java
+++ src/DatosEstudiantes.java
@@ -1,25 +1,21 @@
 
 import java.util.Scanner;
 public class DatosEstudiantes {
-    public static void main(String[] args){
-        Scanner sr = new Scanner(System.in);
+    public static void main ( String [] args ) {
+        Scanner ingreso = new Scanner ( System.in ) ;
         ///Ingresamos los PAOS
-        System.out.println("Ingrese el PAO a calificar");
-        int pao = sr.nextInt();
+        System.out.println ("Ingrese el PAO a calificar " ) ;
+        int pao = ingreso.nextInt () ;
 
         /// Ingresamos el numero de estudiantes
-        System.out.println("Ingrese el numero de estudiantes del PAO " + pao + ":");
-        int cantidad = sr.nextInt();
+        System.out.println ( "Ingrese el numero de estudiantes del PAO " + pao + ":" );
+        int cantidad = ingreso.nextInt () ;
 
         /// Ingresamos el nombre de los estudiantes
-        System.out.println("Ingrese el nombre del estudiante : ");
-        String [] NombreEstudiantes= new String[cantidad];
-        for (int i; i<cantidad ; i++){
-
+        System.out.println ("Ingrese el nombre del estudiante :") ;
+        String [] NombreEstudiantes = new String [cantidad] ;
+        for (int i=0 ; i < cantidad ; i++) {
+            String nombre = ingreso.next ();
         }
-       
-
-
     }
-    
 }
