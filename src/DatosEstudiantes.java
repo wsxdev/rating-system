6caa2abb50b819ddad678387195616cf33759983
Britanr <redrovanbritay1751623776gmail.com>
2025-04-09 23:05:02 -0500

    Datos.Estudiantes,.java " Cambio en nom. de Estudiantes"

--- src/DatosEstudiantes.java
+++ src/DatosEstudiantes.java
@@ -1,21 +1,23 @@
 
 import java.util.Scanner;
+
 public class DatosEstudiantes {
-    public static void main ( String [] args ) {
-        Scanner ingreso = new Scanner ( System.in ) ;
+    public static void main(String[] args) {
+        Scanner ingreso = new Scanner(System.in);
         ///Ingresamos los PAOS
-        System.out.println ("Ingrese el PAO a calificar " ) ;
-        int pao = ingreso.nextInt () ;
+        System.out.println("Ingrese el PAO a calificar: ");
+        int pao = ingreso.nextInt();
 
         /// Ingresamos el numero de estudiantes
-        System.out.println ( "Ingrese el numero de estudiantes del PAO " + pao + ":" );
-        int cantidad = ingreso.nextInt () ;
+        System.out.println("Ingrese el numero de estudiantes del PAO " + pao + ":");
+        int cantidad = ingreso.nextInt();
 
         /// Ingresamos el nombre de los estudiantes
-        System.out.println ("Ingrese el nombre del estudiante :") ;
-        String [] NombreEstudiantes = new String [cantidad] ;
-        for (int i=0 ; i < cantidad ; i++) {
-            String nombre = ingreso.next ();
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Ingrese el nombre del estudiante " + (i + 1) + " :");
+            String[] nombreEstudiantes = new String[cantidad];
+            String nombre = ingreso.next();
         }
     }
 }
