8eef8e7c311e7f17d9202962c2fee7cf22f74cc8
wsxdev <williamsxdev@gmail.com>
2025-04-14 02:49:59 -0500

    Validacion Terminada

--- src/DatosEstudiantes.java
+++ src/DatosEstudiantes.java
@@ -3,28 +3,49 @@ import java.util.Scanner;
 public class DatosEstudiantes {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        ///Ingresamos los PAOS
-        System.out.println("");
-        System.out.println("  //   SISTEMA DE NOTAS    //   ");
-        System.out.println(" ");
+        System.out.println("\n||    BIENVENIDO AL SISTEMA DE NOTAS YANKAY    ||  \n");
+        /// Verificamos e ingresamos los PAOS
         System.out.println("[ SELECCIONE EL PAO ]");
-        System.out.print("   " + "Ingrese el PAO a calificar: ");
-        int pao = entrada.nextInt();
-        System.out.println("");
+        int pao;
+        while (true) {
+            System.out.print("   Ingrese el PAO a calificar (1-8): ");
+            pao = leerNumerosValidos(entrada);
+
+            if (pao >= 1 && pao <= 8) {
+                break;
+            } else {
+                System.out.println("\n  ------------------------------------");
+                System.out.println("  ERROR: el PAO debe estar entre 1 y 8");
+                System.out.println("  ------------------------------------");
+            }
+        }
+
         /// Ingresamos el numero de estudiantes
-        System.out.println("[ INGRESO DE ESTUDIANTES ]");
-        System.out.print("   " + "Ingrese el numero de estudiantes del PAO " + pao + ": ");
-        int cantidad = entrada.nextInt();
-        System.out.println(" ");
-        System.out.println("[ NOMBRES DE LOS ESTUDIANTES ]");
+        System.out.println("\n[ INGRESO DE ESTUDIANTES ]");
+        int cantidadEstudiantes;
+        while (true) {
+            System.out.print("   Ingrese el numero de estudiantes del PAO " + pao + ": ");
+            cantidadEstudiantes = leerNumerosValidos(entrada);
+            if (cantidadEstudiantes > 0) {
+                break;
+            } else {
+                System.out.println("\n  ---------------------------------------");
+                System.out.println("  ERROR: ingrese un valor numérico valido");
+                System.out.println("  Intente de nuevo");
+                System.out.println("  ---------------------------------------");
+            }
+        }
+        
+        System.out.println("\n[ NOMBRES DE LOS ESTUDIANTES ]");
+
         /// Ingresamos el nombre de los estudiantes
-        String[] nombreEstudiantes = new String[cantidad];
-        for (int i = 0; i < cantidad; i++) {
-            System.out.print("   " + "Nombre del estudiante " + (i + 1) + ": ");
+        String[] nombreEstudiantes = new String[cantidadEstudiantes];
+        for (int i = 0; i < cantidadEstudiantes; i++) {
+            System.out.print("   Nombre del estudiante " + (i + 1) + ": ");
             nombreEstudiantes[i] = entrada.next();
         }
         System.out.println("");
-        ///Ingresamos las materias de cada PAO
+        /// Ingresamos las materias de cada PAO
         String[] materias = null;
         switch (pao) {
             case 1:
@@ -77,15 +98,14 @@ public class DatosEstudiantes {
                         " FORMULACIÓN TRABAJO DE TITULACIÓN", " TRABAJO DE TITULACIÓN" };
                 break;
             default:
-                System.out.println(" Error, ingrese el PAO correcto del 1 al 8");
-                return; // Para que se salga del método actual
+                break;
         }
         ///Matriz para agregar las notas
-        double[][] notas = new double[cantidad][materias.length];
+        double[][] notas = new double[cantidadEstudiantes][materias.length];
         System.out.println("[ INGRESO DE NOTAS ]");
-        for (int i = 0; i < cantidad; i++) {
-            System.out.println("");
-            System.out.println("  Estudiante: " + nombreEstudiantes[i]);
+        for (int i = 0; i < cantidadEstudiantes; i++) {
+            System.out.println("  \nEstudiante: " + nombreEstudiantes[i]);
+            // ASIGNAR NOTAS DE ACUERDO A LAS MATERIAS 
             for (int j = 0; j < materias.length; j++) {
                 System.out.print("    >" + materias[j] + ": ");
                 // Fíjese aquí, al momento de leer las notas, se llama al método leerNotaValida
@@ -98,29 +118,48 @@ public class DatosEstudiantes {
 
     // MÉTODO PARA VALIDAR DATOS DE ENTRADA
     // Este método recibe un parámetro tipo Scanner, llamado entradaValida
-    public static double leerNotaValida(Scanner entradaValida, String materiaTemporal) {
+    public static double leerNotaValida(Scanner entradaNotaValida, String materiaTemporal) {
         double nota;
         while (true) {
-            if (entradaValida.hasNextDouble()) { // hasNextDouble verfica si lo ingresado es válido
-                nota = entradaValida.nextDouble();
+            if (entradaNotaValida.hasNextDouble()) { // hasNextDouble verfica si lo ingresado es válido
+                nota = entradaNotaValida.nextDouble();
                 if (nota >= 0 && nota <= 10) {
                     break;
                 } else {
-                    System.out.println("  -------------------");
-                    System.out.println("   ERROR: la nota debe estar entre 0 y 10");
-                    System.out.println("   Intente de nuevo");
-                    System.out.println("  -------------------");
+                    System.out.println("\n  --------------------------------------");
+                    System.out.println("  ERROR: la nota debe estar entre 0 y 10");
+                    System.out.println("  Intente de nuevo");
+                    System.out.println("  --------------------------------------");
                     System.out.print("    >" + materiaTemporal + ": ");
                 }
             } else {
-                System.out.println("  -------------------");
-                System.out.println("   ERROR: ¡La nota debe ser un NÚMERO! :/");
-                System.out.println("   Intente de nuevo ");
-                System.out.println("  -------------------");
+                System.out.println("\n  --------------------------------------");
+                System.out.println("  ERROR: ¡La nota debe ser un NÚMERO! :/");
+                System.out.println("  Intente de nuevo");
+                System.out.println("  --------------------------------------");
                 System.out.print("    >" + materiaTemporal + ": ");
-                entradaValida.next();
+                entradaNotaValida.next();
             }
         }
         return nota;
     }
+
+    // MÉTODO PARA VALIDAR DATOS NUMÉRICOS
+    public static int leerNumerosValidos(Scanner entradaNumerica){
+        int numero;
+        while (true) {
+            if (entradaNumerica.hasNextInt()) {
+                numero = entradaNumerica.nextInt();
+                break;
+            } else {
+                System.out.println("\n  ---------------------------------------");
+                System.out.println("  ERROR: ingrese un valor numérico válido");
+                System.out.println("  ---------------------------------------");
+                System.out.print("  Intente de nuevo: ");
+                entradaNumerica.next();
+            }
+        }
+        return numero;
+    }
+
 }
